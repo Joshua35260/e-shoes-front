c988e9d917b265ac7aaeb46397e7efe2db7ca24e
Joshua <joshua35260@gmail.com>
2022-09-20 17:58:20 +0200

    navbar début

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,17 @@
-import React from "react";
+import { NavLink, Link } from "react-router-dom";
+import facebookImg from "../assets/images/facebook.png";
+import instagramImg from "../assets/images/instagram2.png";
+import logo from "../assets/images/logo.png";
 
 const Footer = () => {
   return (
-    <div className="footerContainer">
-      <div className="footer">
-        I am footer, but people don't see mee as usual :((
-      </div>
-    </div>
+    <>
+      <footer>
+        <div className="footerContainer">
+          <div className="footer">footer</div>
+        </div>
+      </footer>
+    </>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,74 @@
-import React from "react";
-
+import { NavLink } from "react-router-dom";
+import * as Icon from "react-feather";
+import logo from "../assets/images/logo.png";
 const Navbar = () => {
   return (
-    <div className="navbarContainer">
-      <div className="navbar">Hello World !</div>
-    </div>
+    <header>
+      <div className="navbarContainer">
+        <div className="firstNav">
+          <NavLink to="/">
+            <img src={logo} className="logo" alt="logo E-sneaker.com" />
+          </NavLink>
+          <input
+            type="text"
+            className="inputNav"
+            placeholder="Rechercher un produit, une marque"
+          />
+        </div>
+        <div className="secondNav">
+          <NavLink
+            to="/homme"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>HOMME</div>
+          </NavLink>
+          <NavLink
+            to="/femme"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>FEMME</div>
+          </NavLink>
+          <NavLink
+            to="/enfant"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>ENFANT</div>
+          </NavLink>
+          <NavLink
+            to="/brand"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>MARQUES</div>
+          </NavLink>
+        </div>
+        <div className="thirdNav">
+          <NavLink
+            to="/login"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>
+              <Icon.User size={28} />
+            </div>
+          </NavLink>
+          <NavLink
+            to="/fav"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>
+              <Icon.Heart size={28} />
+            </div>
+          </NavLink>
+          <NavLink
+            to="/cart"
+            className={({ isActive }) => (isActive ? "active" : undefined)}
+          >
+            <div>
+              <Icon.ShoppingCart size={28} />
+            </div>
+          </NavLink>
+        </div>
+      </div>
+    </header>
   );
 };
 
